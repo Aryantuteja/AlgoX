a8753918d0e981a1f7419ba273793647ad060e9f
Aryantuteja <54592713+Aryantuteja@users.noreply.github.com>
2021-07-26 10:28:29 +0530

    Update bubble_sort.cpp

--- SortX/bubble_sort.cpp
+++ SortX/bubble_sort.cpp
@@ -6,13 +6,13 @@ void bubblesort(int a[],int n)
     int i,j,temp,mini;
     for(i=0;i<n;i++){
       
-        for(j=0;j<i;j++)
+        for(j=0;j<n-i-1;j++)
         {
-            if(a[i]<a[j])
+            if(a[j]>a[j+1])
             {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
